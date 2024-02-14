662379854eca77779f5ac8659395f8a1c9f346ec
unknown <dlee9370@gmail.com>
2024-02-14 21:45:43 +0300

    update

--- model_prep/inference.py
+++ model_prep/inference.py
@@ -1,4 +1,5 @@
 from transformers import AutoModel, AutoTokenizer
+from sklearn.metrics.pairwise import cosine_similarity
 from model_prep.utils import *
 from annoy import AnnoyIndex
 import pandas as pd
@@ -17,5 +18,18 @@ def get_chatbot_response(query):
 
     nearest_neighbors = index.get_nns_by_vector(query_embed[0], n=3)
     # choice = np.random.choice(nearest_neighbors, size=1)[0]
-    response = df.iloc[nearest_neighbors[0]]['original_response']
+    found_sentences = []
+    idx = []
+    for i in nearest_neighbors:
+        idx.append(i)
+        found_sentences.append(df.iloc[i]['context'])
+
+    pooled_embeds = encode(found_sentences, tokenizer, model, device)
+    pooled_embeds = pooled_embeds.cpu().detach().numpy()
+    query_embed = query_embed.detach().numpy()
+
+    cosine_similarities = cosine_similarity(query_embed, pooled_embeds).flatten()
+    relevant_indices = np.argsort(cosine_similarities, axis=0)[::-1][:5]
+
+    response = df.iloc[idx[relevant_indices[0]]]['original_response']
     return response
