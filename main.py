99bfd17f460c99da4e229e90fb52fc2cf3496b84
unknown <dlee9370@gmail.com>
2024-02-18 00:10:37 +0300

    update

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ class BotMessage(BaseModel):
 
 
 app = FastAPI()
-
+all_context = ''
+count = 0
 
 @app.get("/")
 async def open_main_page():
+    global all_context
+    global count
+    all_context = ''
+    count = 0
     return FileResponse("web/index.html")
 
 
 # Endpoint to receive bot messages
 @app.post("/bot/message")
 async def receive_bot_message(bot_message: BotMessage):
-    answer = get_chatbot_response(bot_message.message)
+    global all_context
+    global count
+    answer, new_context = get_chatbot_response(bot_message.message, all_context, count)
+    all_context = new_context
+    count += 1
+
     return {"bot_response": answer}
--- model_prep/inference.py
+++ model_prep/inference.py
@@ -4,7 +4,7 @@ from model_prep.utils import *
 from annoy import AnnoyIndex
 import pandas as pd
 
-model = BertModel.from_pretrained('models/sbert_1e')
+model = BertModel.from_pretrained('models/sbert_2e')
 tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
 index = AnnoyIndex(EMBEDDING_SIZE)
@@ -12,31 +12,19 @@ index.load('data/show_context.tree')
 
 df = pd.read_csv('data/prepared_with_context+label+negative.csv')
 df = df[df['label'] == 1] # get only true answer - response rows
-all_context = ''
-count = 0
+df.reset_index(drop=True, inplace=True)
 
-def get_chatbot_response(query):
-    global all_context
-    if (count == 0) or (count > 5):
+def get_chatbot_response(query, all_context, count):
+    if (count == 0) or (count % 2 == 0):
         all_context = query
     else:
-        all_context = all_context + '[SEP]' +query
+        all_context = all_context + ' [SEP] ' + query
+    print(all_context)
+
     all_context = clean_symbols(all_context)
     query_embed = encode([all_context], tokenizer, model, 'cpu')
     query_embed = query_embed.detach().numpy()
 
-    nearest_neighbors = index.get_nns_by_vector(query_embed[0], n=3)
-    found_sim_context = []
-    idx = []
-    for i in nearest_neighbors:
-        idx.append(i)
-        found_sim_context.append(df.iloc[i]['context'])
-
-    pooled_embeds = encode(found_sim_context, tokenizer, model, device)
-    pooled_embeds = pooled_embeds.cpu().detach().numpy()
-
-    cosine_similarities = cosine_similarity(query_embed, pooled_embeds).flatten()
-    relevant_indices = np.argsort(cosine_similarities, axis=0)[::-1][:3]
-
-    response = df.iloc[idx[relevant_indices[0]]]['original_response']
-    return response
+    nearest_neighbors = index.get_nns_by_vector(query_embed[0], n=1)
+    response = df.iloc[nearest_neighbors[0]]['original_response']
+    return response, all_context + ' [SEP] ' + response
